8c5b62f7851593f2574439f949a63ed3b1880f22
plima94 <pauloalima1994@gmail.com>
2023-01-30 14:51:30 +0000

    added collision with food

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
-
+from food import Food
 
 screen = Screen()
-screen.setup(width=600,height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
@@ -13,17 +13,16 @@ starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 segments = []
 
-#Creates Snake
+# Creates Snake
 snake = Snake()
+food = Food()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
-
-
 
 game_is_on = True
 while game_is_on:
@@ -33,23 +32,8 @@ while game_is_on:
     # moves Snake
     snake.move()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
